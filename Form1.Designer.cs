8a54a6c2c56b9d71461b8b1e7ae12b2970f65e3b
Emmanuel Pereira <emmanuel@timais.com>
2023-07-13 14:08:54 -0300

    Projeto

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace FileUtility
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnOrig = new System.Windows.Forms.Button();
+            this.btnDest = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(366, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Replace";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(264, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(330, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(264, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // btnOrig
+            // 
+            this.btnOrig.Location = new System.Drawing.Point(10, 12);
+            this.btnOrig.Name = "btnOrig";
+            this.btnOrig.Size = new System.Drawing.Size(266, 20);
+            this.btnOrig.TabIndex = 3;
+            this.btnOrig.Text = "...";
+            this.btnOrig.UseVisualStyleBackColor = true;
+            this.btnOrig.Click += new System.EventHandler(this.btnOrig_Click);
+            // 
+            // btnDest
+            // 
+            this.btnDest.Location = new System.Drawing.Point(328, 12);
+            this.btnDest.Name = "btnDest";
+            this.btnDest.Size = new System.Drawing.Size(266, 20);
+            this.btnDest.TabIndex = 4;
+            this.btnDest.Text = "...";
+            this.btnDest.UseVisualStyleBackColor = true;
+            this.btnDest.Click += new System.EventHandler(this.btnDest_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(273, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = ">>>";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 97);
+            this.Controls.Add(this.btnDest);
+            this.Controls.Add(this.btnOrig);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Replace in Flies";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnOrig;
+        private System.Windows.Forms.Button btnDest;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace FileUtility
+{
+    public partial class Form1 : Form
+    {
+        private string origem;
+        private string destino;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string MyProperty { get; set; }
+
+        private static void ReplaceTextInFiles(DirectoryInfo directory, string term, string replace)
+        {
+            int tentativas = 0;
+            inicio:
+            ReplaceInFiles(directory, term, replace);
+            var dirs = directory.GetDirectories("*", SearchOption.AllDirectories);
+            foreach (var dir in dirs)
+            {
+                try
+                {
+                    if (dir.Name.Contains(term))
+                        Directory.Move(dir.FullName, dir.Parent.FullName + "/" + dir.Name.Replace(term, replace));
+                }
+                catch
+                {
+                    if (tentativas < 3)
+                    {
+                        tentativas++;
+                        goto inicio;
+                    }
+                }
+            }
+        }
+
+        static bool IsBinary(string content)
+        {
+            var bytes = Encoding.ASCII.GetBytes(content);
+            for (int i = 0; i < bytes.Length; i++)
+                if (bytes[i] > 127)
+                    return true;
+            return false;
+        }
+
+        public static void ReplaceInFiles(DirectoryInfo dir, string term, string replace)
+        {
+            foreach (var file in dir.GetFiles("*", SearchOption.AllDirectories))
+            {
+                if (
+                    file.Extension != ".dll" 
+                    && file.Extension != ".dylib" 
+                    && file.Extension != ".so" 
+                    && file.Extension != ".pdb" 
+                    && file.Extension != ".cache")
+                {
+                    try
+                    {
+
+                        string strFile = File.ReadAllText(file.FullName);
+
+                        if (IsBinary(strFile))
+                            continue;
+
+                        strFile = Regex.Replace(strFile, $@"{term}", replace);
+                        File.WriteAllText(file.FullName, strFile);
+
+                        if (file.Name.Contains(term))
+                            Directory.Move(file.FullName, file.Directory + "/" + file.Name.Replace(term, replace));
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+
+                    }
+                }
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (origem != null && destino != null && CopiarDiretorios(origem, destino))
+            {
+                var dir = new DirectoryInfo(destino);
+                ReplaceTextInFiles(dir, textBox1.Text, textBox2.Text);
+            }
+        }
+
+        private bool CopiarDiretorios(string origem, string destino)
+        {
+            if (Directory.Exists(destino))
+            {
+                if (MessageBox.Show("O diretorio de destino ja existe, continuar?", "!!!!", MessageBoxButtons.OKCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.OK)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            foreach (string dir in Directory.GetDirectories(origem, "*", SearchOption.AllDirectories))
+            {
+                Directory.CreateDirectory(Path.Combine(destino, dir.Substring(origem.Length + 1)));
+            }
+
+            foreach (string file_name in Directory.GetFiles(origem, "*", SearchOption.AllDirectories))
+            {
+                File.Copy(file_name, Path.Combine(destino, file_name.Substring(origem.Length + 1)));
+            }
+            return true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOrig_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            var result = dlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                var di = new DirectoryInfo(dlg.SelectedPath);
+                this.origem = dlg.SelectedPath;
+                this.textBox1.Text = di.Name;
+                btnOrig.Text = this.origem;
+            }
+        }
+
+        private void btnDest_Click(object sender, EventArgs e)
+        {
+            var dlg = new SaveFileDialog();
+            var result = dlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                var fi = new FileInfo(dlg.FileName);
+                this.destino = fi.FullName;
+                textBox2.Text = fi.Name;
+                btnDest.Text = this.destino;
+            }
+        }
+    }
+}
